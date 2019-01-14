4e52f4eb249e70b7d1136d50408347d0f88a48fe
Killercat <rayershih@gmail.com>
2019-01-14 23:09:54 +0800

    BB-16 Data Controller for slash command (Requester)
    Signed-off-by: Killercat <rayershih@gmail.com>

--- BossBot/SlashCmdHandler.go
+++ BossBot/SlashCmdHandler.go
@@ -7,15 +7,16 @@ import (
 	"github.com/nlopes/slack/slackevents"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
-	"net/http"
+	"strconv"
+	"time"
 )
 
 type MsgSchedulerController struct {
-	config             Configuration
-	messageBroadcaster MessageBroadcaster
+	config Configuration
+	//messageBroadcaster MessageBroadcaster
 }
 
-func (msc *MsgSchedulerController) HandleItem(w http.ResponseWriter, msgAct slackevents.MessageAction) (slack.WebhookMessage, error) {
+func (msc *MsgSchedulerController) HandleResponse(msgAct slackevents.MessageAction) (slack.WebhookMessage, error) {
 
 	if msgAct.CallbackId != "MsgSchOperation" {
 		return slack.WebhookMessage{Text: "Not correct handler"}, errors.Errorf("Not correct handler")
@@ -29,9 +30,8 @@ func (msc *MsgSchedulerController) HandleItem(w http.ResponseWriter, msgAct slac
 		log.Warnln(err)
 	}
 
-	mb := msc.messageBroadcaster
+	mb := MessageBroadcaster{msc.config}
 	whm := slack.WebhookMessage{}
-	w.Header().Set("Content-Type", "application/json")
 
 	switch schAction.Action {
 	case "invoke":
@@ -67,4 +67,184 @@ func (msc *MsgSchedulerController) HandleItem(w http.ResponseWriter, msgAct slac
 		break
 	}
 	return whm, nil
+}
+
+func (msc *MsgSchedulerController) HandleRequest() (slack.WebhookMessage, error) {
+
+	log.Debugf("Incoming cmd bb_broadcast_list")
+
+	dm, err := GetDataManager(&msc.config)
+
+	errMsg := slack.WebhookMessage{}
+
+	if err != nil {
+		log.Errorf("%s : %s", err, "Error getting data manager!")
+		errMsg.Text = "Error getting data manager!"
+		return errMsg, err
+	}
+
+	bdList, err := dm.GetBroadcastList()
+
+	if err != nil {
+		log.Warnf("%s : %s", err, "Error getting Broadcast List")
+		errMsg.Text = "Error getting Broadcast List!"
+		return errMsg, err
+	}
+
+	ret := slack.WebhookMessage{Text: "Here is broadcast items in system :"}
+
+	for _, broadcast := range bdList {
+
+		var actions []slack.AttachmentAction
+
+		var actionBtnName string
+		var color string
+		value := MsgScheduleIdActions{
+			ScheduleItemId: broadcast.Id,
+		}
+
+		if broadcast.Active == 1 {
+			actionBtnName = "Disable"
+			color = "#3AA3E3"
+			value.Action = "disable"
+		} else {
+			actionBtnName = "Enable"
+			color = "#FF0000"
+			value.Action = "enable"
+		}
+
+		valueJson, err := json.Marshal(&value)
+
+		if err != nil {
+			log.Warnf("Error marshalling json : %+v\n", value)
+			errMsg.Text = "Error marshalling json"
+			return errMsg, err
+		}
+
+		actions = append(actions, slack.AttachmentAction{
+			Name:  actionBtnName,
+			Text:  actionBtnName,
+			Type:  "button",
+			Style: "primary",
+			Value: string(valueJson),
+		})
+
+		value.Action = "invoke"
+		valueJson, err = json.Marshal(&value)
+
+		if err != nil {
+			log.Warnf("Error marshalling json : %+v\n", value)
+			errMsg.Text = "Error marshalling json"
+			return errMsg, err
+		}
+		actions = append(actions, slack.AttachmentAction{
+			Name:  "Invoke Now",
+			Text:  "Invoke Now",
+			Type:  "button",
+			Style: "danger",
+			Value: string(valueJson),
+		})
+
+		var fields []slack.AttachmentField
+
+		//Makeup id / message_id / Message field
+		fields = append(fields, slack.AttachmentField{
+			Title: "Scheduler ID",
+			Value: strconv.Itoa(broadcast.Id),
+			Short: true,
+		})
+
+		fields = append(fields, slack.AttachmentField{
+			Title: "Message ID",
+			Value: strconv.Itoa(broadcast.MessageId),
+			Short: true,
+		})
+
+		fields = append(fields, slack.AttachmentField{
+			Title: "Message",
+			Value: broadcast.Message,
+			Short: false,
+		})
+
+		//Start date and End date
+
+		fields = append(fields, slack.AttachmentField{
+			Title: "Start From",
+			Value: func() string {
+				if broadcast.StartDate.Valid {
+					return broadcast.StartDate.Time.Format(time.RFC822)
+				}
+				return "Not set"
+			}(),
+			Short: true,
+		})
+
+		fields = append(fields, slack.AttachmentField{
+			Title: "Ends at",
+			Value: func() string {
+				if broadcast.EndDate.Valid {
+					return broadcast.StartDate.Time.Format(time.RFC822)
+				}
+				return "Not set"
+			}(),
+			Short: true,
+		})
+
+		//Recursive date time
+
+		fields = append(fields, slack.AttachmentField{
+			Title: "Run at nth day of month",
+			Value: func() string {
+				if broadcast.Day.Valid {
+					return "Every " + strconv.Itoa(int(broadcast.Day.Int64)) + " of the month"
+				}
+				return "Not set"
+			}(),
+			Short: true,
+		})
+
+		fields = append(fields, slack.AttachmentField{
+			Title: "Run at day in week",
+			Value: func() string {
+				if broadcast.WeekDay.Valid {
+					return "Every " + time.Weekday(broadcast.WeekDay.Int64).String()
+				}
+				return "Not set"
+			}(),
+			Short: true,
+		})
+
+		fields = append(fields, slack.AttachmentField{
+			Title: "Broadcast Time",
+			Value: broadcast.BroadcastTime,
+			Short: true,
+		})
+
+		fields = append(fields, slack.AttachmentField{
+			Title: "Channel",
+			Value: broadcast.ChannelName,
+			Short: true,
+		})
+
+		attachment := slack.Attachment{
+			Text:       "Message detail",
+			Actions:    actions,
+			Color:      color,
+			CallbackID: "MsgSchOperation",
+			Fields:     fields,
+		}
+
+		if ret.Attachments == nil {
+			ret.Attachments = []slack.Attachment{}
+		}
+
+		ret.Attachments = append(ret.Attachments, attachment)
+
+	}
+
+	return ret, nil
+
+	//ret, err := json.Marshal(params)
+	//w.Header().Set("Content-Type", "application/json")
+	//_, _ = w.Write(ret)
 }
